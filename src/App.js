472d2ada9d09b2d1887728b0ca62839e3574191d
LuizWebDev <luizwagnermelo@hotmail.com>
2022-04-27 12:46:48 +0100

    Added a route for search

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import {Route} from 'react-router-dom'
 import NavBar from './components/NavBar/Index'
 import './App.css';
 import Welcome from './Pages/Welcome/Index';
+import Search from './Pages/Search/Index';
 
 function App() {
   return (
 <>
 <NavBar />
+
 <Routes>
 <Route path="/" element={<Welcome />}/ >
+<Route path="search" element={<Search />}/ >
 </Routes>
     </>
   );
