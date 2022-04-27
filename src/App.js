5b9a30dc32228b5488404c34505e4c6633e44314
LuizWebDev <luizwagnermelo@hotmail.com>
2022-04-27 12:06:09 +0100

    created my first route for welcome page in the App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Routes} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import NavBar from './components/NavBar/Index'
 import './App.css';
+import Welcome from './Pages/Welcome/Index';
 
 function App() {
   return (
-    <>
-   
-    <NavBar />
+<>
+<NavBar />
+<Routes>
+<Route path="/" element={<Welcome />}/ >
+</Routes>
     </>
   );
 }
