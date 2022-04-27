f3bb3980e1ed1e41be623551596b18455ee0f72f
LuizWebDev <luizwagnermelo@hotmail.com>
2022-04-27 12:40:29 +0100

    created a simple page for search

---
+++ src/Pages/Search/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Style.css'
+
+const Search = () => {
+  return (
+    <>
+    <p className='big-text'>Search Here</p>
+    <p><input type="text" /></p>
+    </>
+  )
+}
+
+export default Search
