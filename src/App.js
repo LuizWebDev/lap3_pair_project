29fb8f6a1e83c439331c890c66fe8265b9f6f85d
LuizWebDev <luizwagnermelo@hotmail.com>
2022-04-27 11:27:03 +0100

    imported NavBar to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import NavBar from './components/NavBar/Index'
 import './App.css';
 
 function App() {
   return (
-    <div id="App">
-    </div>
+    <>
+   
+    <NavBar />
+    </>
   );
 }
 
