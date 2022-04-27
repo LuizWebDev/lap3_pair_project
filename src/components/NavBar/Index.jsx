a15042b1b185042428360cbfbf3aded499b770d6
LuizWebDev <luizwagnermelo@hotmail.com>
2022-04-27 11:24:49 +0100

    Created the NavBar component

---
+++ src/components/NavBar/Index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import './style.css';
+
+
+
+const NavBar = () => {
+
+ 
+        // const goBack = useNavigate();
+     
+
+
+    return (
+        <nav>
+           <a href='#'>Home</a>
+           <a href='#'>Home</a>
+           
+        </nav>
+    );
+    }
+
+export default NavBar;
